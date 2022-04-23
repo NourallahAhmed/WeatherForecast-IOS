4cd7f68205f99e09bdf80cf9cebe75e445109be3
NourallahAhmed <87453033+NourallahAhmed@users.noreply.github.com>
2022-04-23 16:15:42 -0700

    Initial Commit

---
+++ WeatherForecast2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherForecast2
+//
+//  Created by NourAllah Ahmed on 4/23/22.
+//  Copyright © 2022 NourAllah Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
